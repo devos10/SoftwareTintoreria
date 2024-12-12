7fb3c909d791335286254dc7822bc0b83d34ad81
Osbaldo Velazquez <osbaldo081004@gmail.com>
2024-12-12 11:04:13 -0600

    Se arreglaron los conflictos en los documentos

--- db.php
+++ db.php
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 <?php	
 //aqui colocaremos la conexion a la base de datos 
-=======
-<?php	
-//aqui colocaremos la conexion a la base de datos 
->>>>>>> fa5ffa37e03aee8eb18402c3c909c1d42bfd39d7
+
--- footer.php
+++ footer.php
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-
-
 <footer>
     <p>&copy; <?php echo date("Y"); ?> Lavandería Punto de Venta. Todos los derechos reservados.</p>
     <img src="imagenes/Logo.png" 
@@ -10,18 +7,3 @@
     <p>Contacto: <a href="mailto:soporte@lavanderia.com">soporte@lavanderia.com</a> | Tel: 123-456-7890</p>
     
 </footer>
-
-=======
-
-
-<footer>
-    <p>&copy; <?php echo date("Y"); ?> Lavandería Punto de Venta. Todos los derechos reservados.</p>
-    <img src="imagenes/Logo.png" 
-        width="100"
-        height="100"/>
-    <h4>ConsultoresCY02</h4>
-    <p>Contacto: <a href="mailto:soporte@lavanderia.com">soporte@lavanderia.com</a> | Tel: 123-456-7890</p>
-    
-</footer>
-
->>>>>>> fa5ffa37e03aee8eb18402c3c909c1d42bfd39d7
