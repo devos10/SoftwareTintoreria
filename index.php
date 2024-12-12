df3fbc6ec8a3cb1d427e23e517c0d66ad7adcba4
devos10 <111657011+devos10@users.noreply.github.com>
2024-12-11 23:03:14 -0600

    Segundo Commit-Avances 1.1
    Se arreglo el menu: 
    Menu
    -RegistroCliente
    -Registro Orden 
    -Impresion ticket
    -Gestion de Prendas
    -Reporte 
    Se coloco las vistas de:
    Registro Cliente y Registro Orden

--- index.php
+++ index.php
@@ -3,23 +3,118 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    
     <title>Tintoreria</title>
 </head>
-<body>
-    <center>
-        <h1>Punto de Venta Tintoreria</h1>
-        <hr/>
-        <p>
-            
-        </p>
-        <h4>CY02</h4>
-        <!--Le indicamos alto y ancho de la imagen-->
-        <img src="imagenes/Logo.png" 
-        width="100"
-        height="100"/>
-        <h4>Consultores-Y02</h4>
+<body id=body>
+        <div>
+            <h1 id=TituloTintoreria>Tintoreria</h1>
+            <!--Le indicamos alto y ancho de la imagen-->
+            <img id=logo src="imagenes/Logo.png" 
+            width="80"
+            height="80"/>
+            <img id=inicio src="imagenes/inicio.png" 
+            width="80"
+            height="80"/>
+        </div>
+        <!--Vista del menu-->
+        <div id=menu>
+                <label id=tituloMenu>Menú</label>
+            <div id= registro>
+                <a href="registro.php" class="enlace">
+                <img id=registro src="imagenes/Registro.png"
+                    width="80"
+                    height="80"/>
+                <label id=RCtitulo>Registro Cliente</label>
+                </a>
+            </div>
 
-    </center>
+            <div id=registroOrden>
+                <a href="pedidos.php" class="enlace">
+                    <img id=registroorden src="imagenes/RegistroOrden.png"
+                        width="80"
+                        height="80"
+                        />
+                    <label id=ROtitulo>Registro Orden</label>
+                </a>
+            </div>
+
+            <div id=ticket>
+                <a href=ticket.php class="enlace">
+                    <img id=ticket src="imagenes/ticket.png"
+                        width="80"
+                        height="80"
+                    />
+                <label id=ITtitulo>Impresion Ticket</label>
+                </a>
+            </div>
+
+            <div id=gestion>
+            <a href=gestion.php class="enlace">
+                    <img id=gestion src="imagenes/gestion.png"
+                        width="80"
+                        height="80"
+                    
+                    />
+                    <label id=GestiondePrendas>Gestion de Prendas</label>
+
+                </a>
+            </div>
+
+            <div id=reporte>
+                <a href=reportes.php class="enlace">
+                    <img id=reporte src="imagenes/reporte.png"
+                        width="70"
+                        height="80"
+                    
+                    />
+                    <label id=Reportestitulo>Reportes</label>
+
+                </a>
+
+            </div>
+        </div>
+        
+
+        <div id=Servicios>
+
+            <label id=TituloServicios>Servicios</label>
+
+            <div id=Stintoreria>
+                <img id=servicio1 src="imagenes/Tintoreria.png"
+                width="200"
+                height="200"/>
+                <label id=Ttitulo>Tintoreria</label>
+
+            </div>
+
+            <div id=Slavado>
+                <img id= Servicio2 src="imagenes/lavado.png"
+                width="150"
+                height="150"
+                />
+                <label id=Lavadotitulo>Lavado</label>
+            </div>
+
+            <div id=Splanchado>
+                <img id=Servicio3 src="imagenes/planchado.png"
+                width="150"
+                height="150"
+                />
+                <label id=Planchadotitulo>Planchado</label>
+
+            </div>
+
+        </div>
+        
+    <div id=footer>
+        <!-- Incluir el Footer -->
+        <?php include 'footer.php'; ?>
+    
+
+    </div>
+    
     
 
 </body>
--- pedidos.php
+++ pedidos.php
@@ -3,42 +3,139 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro de Pedido</title>
+    <title>Registro de Orden</title>
+    <link rel="stylesheet" href="css/pedidos.css">
 </head>
-<body>
-    <h1>Registro de Pedido</h1>
+<body id=body>
+    <h1 id=TituloRegistrodeOrden>Registro de Orden</h1>
+    <div id=imgRegistroOrden>
+        <img src="imagenes/RegistroOrden.png" 
+                width="90"
+                height="90"/>
+
+    </div>
+    <a href="index.php" class="enlace">
+        <img id=imgReturn src="imagenes/return.png"
+                width="40"
+                height="40"
+        />
+
+    </a>
+    
+            <!--Vista del menu-->
+            <div id=menu>
+                <label id=tituloMenu>Menú</label>
+            <div id= registro>
+                <a href="registro.php" class="enlace">
+                <img id=registro src="imagenes/Registro.png"
+                    width="80"
+                    height="80"/>
+                <label id=RCtitulo>Registro Cliente</label>
+                </a>
+            </div>
+
+            <div id=registroOrden>
+                <a href="pedidos.php" class="enlace">
+                    <img id=registroorden src="imagenes/RegistroOrden.png"
+                        width="80"
+                        height="80"
+                        />
+                    <label id=ROtitulo>Registro Orden</label>
+                </a>
+            </div>
+
+            <div id=ticket>
+                <a href=ticket.php class="enlace">
+                    <img id=ticket src="imagenes/ticket.png"
+                        width="80"
+                        height="80"
+                    />
+                <label id=ITtitulo>Impresion Ticket</label>
+                </a>
+            </div>
+            <div id=gestion>
+            <a href=gestion.php class="enlace">
+                    <img id=gestion src="imagenes/gestion.png"
+                        width="80"
+                        height="80"
+                    
+                    />
+                    <label id=GestiondePrendas>Gestion de Prendas</label>
+
+                </a>
+            </div>
+            <div id=reporte>
+                <a href=reportes.php class="enlace">
+                    <img id=reporte src="imagenes/reporte.png"
+                        width="70"
+                        height="80"
+                    
+                    />
+                    <label id=Reportestitulo>Reportes</label>
+
+                </a>
+
+            </div>
+        </div>
+
+    <!---Formulario para el pedido-->
     <form action="guardar_pedido.php" method="post">
+        <div id=formulario>
+            <div id=NumerodePrendas>
+                <label id=etiquetaNumerodePrendas>Numero de prendas:</label> 
+                <input type="number" id="inputnumerodeprendas" name="numerodeprendas">
 
-        <label>Numero de prendas</label> 
-        <input type="number" id="numerodeprendas" name="numerodeprendas">
-        <p>
-            <label for="prenda">Prenda</label>
-            <input type="text" id="prenda" name="prenda" required>
+            </div>
+            
+            <div id=Prenda>
+                <p>
+                    <label id=etiquetaPrenda for="prenda">Prenda</label>
+                    <input type="text" id="inputprenda" name="prenda" required>
+                </p>
+            </div>
+            <div id=PrecioUnitario>
 
-            <label for="precio_unitario">Precio Unitario</label>
-            <input type="number" id="precio_unitario" name="precio_unitario" step="0.01" required>
-        </p>
+                    <label id=etiquetaPrecioUnitario for="precio_unitario">Precio Unitario</label>
+                    <input type="number" id="inputprecio_unitario" name="precio_unitario" step="0.01" required>
 
-        <p>
-            <label for="cantidad">Cantidad</label>
-            <input type="number" id="cantidad" name="cantidad" required>
+            </div>
 
-            <label for="precio">Precio</label>
-            <input type="number" id="precio" name="precio">
+            <div id=Precio>
 
-        </p>
+                <label id=etiquetaPrecio for="precio">Precio</label>
+                <input type="number" id="inputprecio" name="precio">
+            </div>
+
+            <div id=Tipos>
+                <label id=etiquetaTipodePrenda for="TiposdePrenda">Tipo de prenda</label>
+                <input type="number" id="inputtipodeprenda" name="cantidad" required>
+
+            </div>
+
+            <div id=Total>
+                <label id=etiquetaTotal for="total">TOTAL</label>
+                <input type="number" id="inputtotal" name="total" step="0.01" readonly>
+
+            </div>
+
+            <div id=Fecha>
+                <label id=etiquetaFechaEstimadadeEntrega for="fecha_entrega">Fecha Estimada de Entrega</label>
+                <input type="date" id="inputfecha_entrega" name="fecha_entrega" required>
+
+            </div>
+            
+
+                <button id="BtnRegistrarPedido" type="submit">Registrar Pedido</button>
+
+
+        </div>
         
-
-        <p>
-            <label for="total">Total</label>
-            <input type="number" id="total" name="total" step="0.01" readonly>
-        </p>
         
-
-        <label for="fecha_entrega">Fecha Estimada de Entrega</label>
-        <input type="date" id="fecha_entrega" name="fecha_entrega" required>
-
-        <button type="submit">Registrar Pedido</button>
     </form>
+    
+        <hr/>
+
+
+    
 </body>
 </html>
